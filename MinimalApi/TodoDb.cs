c0622398d6ef24dee0b289425b38c5a02c66953d
ogabek_toshpolatov <ogabektoshpolatov2002@gmail.com>
2025-08-11 16:51:40 +0500

    Added MinimalApi

---
+++ MinimalApi/TodoDb.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi;
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
